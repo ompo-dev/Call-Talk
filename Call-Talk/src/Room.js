6350974be17392e21d53502eb5464964869ff2b4
Maicon Pereira <106182410+ompo-dev@users.noreply.github.com>
2023-03-21 04:33:56 -0300

    Add files via upload

---
+++ Call-Talk/src/Room.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = 548085722;
+    const serverSecret = "ed08e49096e2052e81f00228f145dfd4";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Seu Nome"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
